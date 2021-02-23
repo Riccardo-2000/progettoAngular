25c3e477c3156e7d7c5713e1997160b304f623b9
RiccardoUccellini <riccardo.uccellini@hotmail.com>
2021-02-23 15:24:51 +0100

    componentsss

--- recupero/src/app/app-routing.module.ts
+++ recupero/src/app/app-routing.module.ts
@@ -7,7 +7,12 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'registrati', loadChildren: () => import('./registrati/registrati.module').then(m => m.RegistratiModule) }
+  { path: 'registrati', loadChildren: () => import('./registrati/registrati.module').then(m => m.RegistratiModule) },
+  { path: 'carrello', loadChildren: () => import('./features/carrello/carrello.module').then(m => m.CarrelloModule) },
+  { path: 'registrati', loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule) },
+  { path: 'registrati', loadChildren: () => import('./features/registrati/registrati.module').then(m => m.RegistratiModule) },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'personalizza', loadChildren: () => import('./features/personalizza/personalizza.module').then(m => m.PersonalizzaModule) }
 ];
 
 @NgModule({
